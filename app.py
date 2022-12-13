e9854387ae449806f2c53f7a648b63dec43a055f
SeoJiWon1 <120435947+SeoJiWon1@users.noreply.github.com>
2022-12-13 16:48:53 +0900

    a

--- app.py
+++ app.py
@@ -11,43 +11,43 @@ st.image(
     width=120,
 )
 
-st.header("J. Webb Space Telescope vs Hubble Telescope")
+st.header("1980's P vs 2020's P")
 
-st.write("")
-"This is a reproduction of the fantastic [WebbCompare](https://www.webbcompare.com/index.html) app by [John Christensen](https://twitter.com/JohnnyC1423). It's built in Streamlit and takes only 10 lines of Python code. If you like this app, please star [John's original repo](https://github.com/JohnEdChristensen/WebbCompare)!"
-st.write("")
+# st.write("")
+# "This is a reproduction of the fantastic [WebbCompare](https://www.webbcompare.com/index.html) app by [John Christensen](https://twitter.com/JohnnyC1423). It's built in Streamlit and takes only 10 lines of Python code. If you like this app, please star [John's original repo](https://github.com/JohnEdChristensen/WebbCompare)!"
+# st.write("")
 
-st.markdown("### Southern Nebula")
-image_comparison(
-    img1="https://www.webbcompare.com/img/hubble/southern_nebula_700.jpg",
-    img2="https://www.webbcompare.com/img/webb/southern_nebula_700.jpg",
-    label1="Hubble",
-    label2="Webb",
-)
+# st.markdown("### Southern Nebula")
+# image_comparison(
+#     img1="https://www.webbcompare.com/img/hubble/southern_nebula_700.jpg",
+#     img2="https://www.webbcompare.com/img/webb/southern_nebula_700.jpg",
+#     label1="Hubble",
+#     label2="Webb",
+# )
 
 
-st.markdown("### Galaxy Cluster SMACS 0723")
-image_comparison(
-    img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
-    img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
-    label1="Hubble",
-    label2="Webb",
-)
+# st.markdown("### Galaxy Cluster SMACS 0723")
+# image_comparison(
+#     img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
+#     img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
+#     label1="Hubble",
+#     label2="Webb",
+# )
 
-st.markdown("### Carina Nebula")
-image_comparison(
-    img1="https://www.webbcompare.com/img/hubble/carina_2800.png",
-    img2="https://www.webbcompare.com/img/webb/carina_2800.jpg",
-    label1="Hubble",
-    label2="Webb",
-)
+# st.markdown("### Carina Nebula")
+# image_comparison(
+#     img1="https://www.webbcompare.com/img/hubble/carina_2800.png",
+#     img2="https://www.webbcompare.com/img/webb/carina_2800.jpg",
+#     label1="Hubble",
+#     label2="Webb",
+# )
 
-st.markdown("### Stephan's Quintet")
-image_comparison(
-    img1="https://www.webbcompare.com/img/hubble/stephans_quintet_2800.jpg",
-    img2="https://www.webbcompare.com/img/webb/stephans_quintet_2800.jpg",
-    label1="Hubble",
-    label2="Webb",
-)
+# st.markdown("### Stephan's Quintet")
+# image_comparison(
+#     img1="https://www.webbcompare.com/img/hubble/stephans_quintet_2800.jpg",
+#     img2="https://www.webbcompare.com/img/webb/stephans_quintet_2800.jpg",
+#     label1="Hubble",
+#     label2="Webb",
+# )
 
 
