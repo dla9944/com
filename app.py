70f89ac298408d7f6e61a308c0b2345c59567667
SeoJiWon1 <120435947+SeoJiWon1@users.noreply.github.com>
2022-12-13 17:42:25 +0900

    3

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ st.set_page_config("1980's P vs 2020's P")
 
 st.image(
     "https://rlyfaazj0.toastcdn.net/20221019/145434.523656000/106827397_1.png", #카메라
-    width=120,
+    width=240,
 )
 
-st.header("1980's P vs 2020's P")
+st.header("1980's Fashion vs 2020's Fashion")
 
 st.write("")
 "This web is a site where you can know the fashion from the past to the present.!"
 st.write("")
 
-st.markdown("### 1980's P")
+st.markdown("### 1980's Fashion")
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
@@ -37,28 +37,28 @@ with col4:
     st.image("https://www.webbcompare.com/img/hubble/southern_nebula_700.jpg")
 
 
-# st.markdown("### Galaxy Cluster SMACS 0723")
-# image_comparison(
-#     img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
-#     img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
-#     label1="Hubble",
-#     label2="Webb",
-# )
+st.markdown("### 2022's Fashion")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
+    img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
 
-# st.markdown("### Carina Nebula")
-# image_comparison(
-#     img1="https://www.webbcompare.com/img/hubble/carina_2800.png",
-#     img2="https://www.webbcompare.com/img/webb/carina_2800.jpg",
-#     label1="Hubble",
-#     label2="Webb",
-# )
+st.markdown("### Carina Nebula")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/carina_2800.png",
+    img2="https://www.webbcompare.com/img/webb/carina_2800.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
 
-# st.markdown("### Stephan's Quintet")
-# image_comparison(
-#     img1="https://www.webbcompare.com/img/hubble/stephans_quintet_2800.jpg",
-#     img2="https://www.webbcompare.com/img/webb/stephans_quintet_2800.jpg",
-#     label1="Hubble",
-#     label2="Webb",
-# )
+st.markdown("### Stephan's Quintet")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/stephans_quintet_2800.jpg",
+    img2="https://www.webbcompare.com/img/webb/stephans_quintet_2800.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
 
 
