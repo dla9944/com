de71b326074e9aad3ba9f8a18fada21cdb0c4f86
SeoJiWon1 <120435947+SeoJiWon1@users.noreply.github.com>
2022-12-13 17:30:10 +0900

    7

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.write("")
 st.write("")
 
 st.markdown("### 1980's P")
-st.columns(2)
+st.columns(2) = col1, col2
 col1.image = "https://www.webbcompare.com/img/hubble/southern_nebula_700.jpg"
 col2.image = "https://cdn.pixabay.com/photo/2018/03/27/17/25/cat-3266673_1280.jpg"
 
